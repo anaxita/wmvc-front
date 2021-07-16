422f1c52a449aab0e43a5098ba6ea30fd957092d
Rovshan <raf-445@mail.ru>
2021-07-16 14:07:00 +0300

    refactoring: fix typography

--- src/components/Servers/Details/Processes/ProcessItem/ProcessItem.jsx
+++ src/components/Servers/Details/Processes/ProcessItem/ProcessItem.jsx
@@ -1,18 +1,18 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-export const ProcessItem = ({processes}) => {
-        return (
-            <div className="process-item" key={processes.name}>
-                <div>{processes.name}</div>
-                <div></div>
-                <div>{processes.cpu_load}</div>
-                <div>{processes.memory}</div>
-                <div className="actions-btn">
+export const ProcessItem = ({ processes }) => {
+    return (
+        <div className="process-item" key={processes.name}>
+            <div>{processes.name}</div>
+            <div>Состояние</div>
+            <div>{processes.cpu_load}</div>
+            <div>{processes.memory}</div>
+            <div className="actions-btn">
                 <button type="button">
                     <FontAwesomeIcon icon="times" />
                 </button>
-                </div>
             </div>
-        )
+        </div>
+    )
 }
