d32ec22507f4ca6b466e090ad0b969ef46d5286b
anaxita <anaxita.me@gmail.com>
2021-07-30 10:28:19 +0300

    add input switch component

---
+++ src/components/Input/Switch.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+export const InputSwitch = () => {
+    return (
+        <label className="checkbox-google">
+        <input type="checkbox" />
+        <span className="checkbox-google-switch"></span>
+        </label>
+    )
+}
