470ea3611518c2c875a16e03a495fcfbc686b5f8
anaxita <anaxita.me@gmail.com>
2021-08-01 11:04:09 +0300

    fix servers handleControl

--- src/components/Servers/ServersList/Item/Item.jsx
+++ src/components/Servers/ServersList/Item/Item.jsx
@@ -19,14 +19,16 @@ export const ServerItem = ({ id, name, hv, state, status, network, cpu_load }) =
     const [error, handleSetError] = useState('')
     const [isLoading, handleSetIsLoading] = useState(false)
 
-    const handleControl = (e) => {
+    const handleControl = (command) => {
         handleSetIsLoading(true);
+
         let body = {
             server_id: id,
             command: '',
         }
+
         let sound = '';
-        switch (e.target.name) {
+        switch (command) {
             case ('power'):
                 if (powerState === 'Off') {
                     body.command = 'start_power'
@@ -40,20 +42,20 @@ export const ServerItem = ({ id, name, hv, state, status, network, cpu_load }) =
                 break;
 
             case ('network'):
-                if (networkState === 'Off') {
-                    body.command = 'start_network'
-                } else {
+                if (networkState) {
                     body.command = 'stop_network'
+                } else {
+                    body.command = 'start_network'
                 }
                 break;
             default:
                 body.command = ''
         }
 
-        handleFetch('post', '/servers/control' , body).then(({ err}) => {
+        handleFetch('post', '/servers/control', body).then(({ err }) => {
 
-            if(!err) {
-                switch (e.target.name) {
+            if (!err) {
+                switch (command) {
                     case ('power'):
                         if (powerState === 'Off') {
                             handleSetState('Running')
@@ -63,41 +65,41 @@ export const ServerItem = ({ id, name, hv, state, status, network, cpu_load }) =
                             sound = sound_server_stopped;
                         }
                         break;
-                        
-                        case ('network'):
-                            if (networkState === 'Off') {
-                                handleSetNetwork('Running')
-                                sound = sound_network_started
-                            } else {
-                                handleSetNetwork('Off')
-                                sound = sound_network_stopped
-                            }
-                            break;
-                            
-                            case ('stop_power_force'):
-                                handleSetState('Off')
-                                sound = sound_server_stopped
-                                break;
-                                
-                                default:
-                                    handleSetState(powerState);
-                                    handleSetNetwork(networkState);
-                                }
-                            } else {
-                                handleSetError(err);
-                                setTimeout(() => {
-                                    handleSetError('');
-                                }, 3000)
-                                sound = sound_server_timeout
-                            }
-                            var audio = new Audio(sound);
-                            audio.play();
-                            handleSetIsLoading(false);
-                        })
+
+                    case ('network'):
+                        if (networkState) {
+                            handleSetNetwork('')
+                            sound = sound_network_stopped
+                        } else {
+                            handleSetNetwork('Running')
+                            sound = sound_network_started
+                        }
+                        break;
+
+                    case ('stop_power_force'):
+                        handleSetState('Off')
+                        sound = sound_server_stopped
+                        break;
+
+                    default:
+                        handleSetState(powerState);
+                        handleSetNetwork(networkState);
+                }
+            } else {
+                handleSetError(err);
+                setTimeout(() => {
+                    handleSetError('');
+                }, 3000)
+                sound = sound_server_timeout
+            }
+            var audio = new Audio(sound);
+            audio.play();
+            handleSetIsLoading(false);
+        })
     }
     const role = () => {
-        if(getUserInfo()) {
-            return getUserInfo().role;    
+        if (getUserInfo()) {
+            return getUserInfo().role;
         } else {
             return 0;
         }
@@ -106,23 +108,25 @@ export const ServerItem = ({ id, name, hv, state, status, network, cpu_load }) =
     return (
         <div className="server-item" id={"server-" + id}>
             {isLoading ? <SpinnerItem /> : null}
-            {error ? <ErrorAbsolute err={error}/> : null}
+            {error ? <ErrorAbsolute err={error} /> : null}
             <div className="srv-name">{name}</div>
             <div className="srv-hv">{hv}</div>
             <div className="srv-state"><span className={"srv-state_name"}>Состояние</span> {(powerState === 'Running') ? <div className="state-on"></div> : <div className="state-off"></div>}</div>
-            <div className="srv-status">{(status === 'Работает нормально' ) ? '' : status}</div>
-            <div className="srv-network"><span className={"srv-state_name"}>Сеть</span> {networkState === "Running" ? <div className="state-on"></div> : <div className="state-off"></div>}</div>
+            <div className="srv-status">{(status === 'Работает нормально') ? '' : status}</div>
+            <div className="srv-network"><span className={"srv-state_name"}>Сеть</span> {networkState ? <div className="state-on"></div> : <div className="state-off"></div>}</div>
             <div className="srv-cpu">{`${cpu_load}%`}</div>
             <div className="actions-btn">
-                    <button  type="button" name="power" onClick={handleControl}><FontAwesomeIcon icon="play-circle" /></button>
-                   <button  type="button" name="network" onClick={handleControl}><FontAwesomeIcon icon="network-wired" /></button>
-                    <button type="button" name="stop_power_force" onClick={handleControl}><FontAwesomeIcon icon="power-off" /></button>
-                    
-                    {role ? <Link to={`/servers/${hv}/${name}/info`}>
-                        <button type="button"><FontAwesomeIcon icon="cog" /></button>
-                    </Link>:
-                        null
-                    }       
+                <button type="button" name="power" onClick={() => handleControl('power')}>
+                    <FontAwesomeIcon icon="play-circle" />
+                </button>
+                <button type="button" name="network" onClick={() => handleControl('network')}><FontAwesomeIcon icon="network-wired" /></button>
+                <button type="button" name="stop_power_force" onClick={() => handleControl('stop_power_force')}><FontAwesomeIcon icon="power-off" /></button>
+
+                {role ? <Link to={`/servers/${hv}/${name}/info`}>
+                    <button type="button"><FontAwesomeIcon icon="cog" /></button>
+                </Link> :
+                    null
+                }
             </div>
         </div>
     )
