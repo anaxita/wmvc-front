e5bfef4d9279ae9d2a0603414ff1e45a11a9574b
anaxita <anaxita.me@gmail.com>
2021-08-02 13:38:39 +0300

    fix state import

--- src/components/Servers/Details/Processes/ProcessItem/ProcessItem.jsx
+++ src/components/Servers/Details/Processes/ProcessItem/ProcessItem.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { handleFetch } from '../../../../Fetch/store';
-import { useState } from 'react/cjs/react.development';
+import { useState } from 'react';
 import { ErrorAbsolute } from '../../../../Error/ErrorAbsolute';
 
 export const ProcessItem = ({ params, processes }) => {
